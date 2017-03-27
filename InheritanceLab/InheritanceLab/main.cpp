37253d4e12667d8f07c7ef4a58d85f2569cb9ea6
Trevis Jontaé Fields <tfields3@student.rcc.edu>
2017-03-27 09:20:21 -0700

    Inheritance Lab

---
+++ InheritanceLab/InheritanceLab/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  InheritanceLab
+//
+//  Created by Trevis Jontaé Fields on 3/27/17.
+//  Copyright © 2017 Trevis Jontaé Fields. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
